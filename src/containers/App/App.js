210ecc63ad8f1aa4a8673299709a99775ecbd146
moseley <jeremymoseley@me.com>
2019-04-16 18:18:46 +0900

    Set basename on BrowserRouter to repo title of frontend

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -6,7 +6,7 @@ import Layout from "../Layout/Layout";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/frontend">
         <div className="App">
           <Layout />
         </div>
