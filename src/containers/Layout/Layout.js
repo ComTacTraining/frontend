9ea29b0af7740e1bc33a63d2f8704b1fd1fa2f62
Jeremy Moseley <moseley@Jeremys-MacBook-Air.local>
2019-05-20 07:08:23 +0900

    Dashboard is front page for demo

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { Route, NavLink } from "react-router-dom";
 
-import Page from "../Page/Page";
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 import Dashboard from "../Dashboard/Dashboard";
-import AddEvolution from "../Dashboard/AddEvolution/AddEvolution";
 import "./Layout.css";
 
 class Layout extends Component {
@@ -15,24 +13,15 @@ class Layout extends Component {
           <nav>
             <ul>
               <li>
-                <NavLink to="/dashboard" exact>
+                <NavLink to="/" exact>
                   Dashboard
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/dashboard/add" exact>
-                  Add Evolution
                 </NavLink>
               </li>
             </ul>
           </nav>
         </header>
-        {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
-        <Route path="/" exact component={Page} />
-        <Route path="/dashboard/add" component={AddEvolution} />
-        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/" exact component={Dashboard} />
         <Route path="/evolutions/:id" component={VideoPlayer} />
-        {/*<Route path="/:page" exact component={Page} />*/}
       </div>
     );
   }
