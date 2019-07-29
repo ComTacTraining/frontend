eddd90dfc0f5a0463ad592e9e372f21d727b91f3
moseley <jeremymoseley@me.com>
2019-07-29 16:22:40 +0900

    Update cognito settings

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
   cognito: {
     REGION: 'us-west-2',
-    USER_POOL_ID: 'us-west-2_dX4EWtZK4',
-    APP_CLIENT_ID: '3hdqikh23ln3ku593ij3vd6s2g',
-    IDENTITY_POOL_ID: 'us-west-2:83d67e57-179b-4d07-abf8-b19592d15115'
+    USER_POOL_ID: 'us-west-2_0QOvAuKl9',
+    APP_CLIENT_ID: '72j28lpr3ne78v476r763c6ln7',
+    IDENTITY_POOL_ID: 'us-west-2:35b463c7-d4a7-4dcf-ba01-fa76f1728313'
   }
 };
