a2d98f2e3bfc3fb5ee2f55d845371412f859063e
moseley <jeremymoseley@me.com>
2019-07-29 15:54:57 +0900

    Update API url

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
   //baseURL: `${process.env.REACT_APP_API_URI}`
-  baseURL: "https://xr3d87synd.execute-api.us-west-2.amazonaws.com/dev"
-  //baseURL: "http://localhost:3000"
+  baseURL: 'https://u2z0rz9asj.execute-api.us-west-2.amazonaws.com/prod/'
 });
 
 export default instance;
