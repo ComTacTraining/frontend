48ad6a13472704b20945674bf31ffd2ac3141278
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-22 05:41:46 +0000

    Deploy ComTacTraining/frontend to github.com/ComTacTraining/frontend.git:gh-pages

---
+++ precache-manifest.4c0b79bcbd96f96e65e50d440019372d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc974ab55b5464ea8b22539b944a5b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34da29250ba06e99c08f",
+    "url": "/static/css/main.7be24717.chunk.css"
+  },
+  {
+    "revision": "623857a7d186e0a469cc",
+    "url": "/static/js/2.d447e769.chunk.js"
+  },
+  {
+    "revision": "34da29250ba06e99c08f",
+    "url": "/static/js/main.7ffcd2b1.chunk.js"
+  },
+  {
+    "revision": "5fb9068a9adec5919db0",
+    "url": "/static/js/runtime-main.6baeedbf.js"
+  },
+  {
+    "revision": "66d9300aed62f11b76c51b196f3958a0",
+    "url": "/static/media/logo.66d9300a.png"
+  },
+  {
+    "revision": "7c1d6adff1d8925d56fcd8d2ab4fc1f4",
+    "url": "/static/media/play.7c1d6adf.svg"
+  }
+]);
